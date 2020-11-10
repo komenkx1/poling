401e41f58e259146f5f7a84435ed89e74f7b8a45
komenkx <komangpermana7@gmail.com>
2020-11-10 17:53:20 +0800

    alhpa

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>Musma Teknik 2020</title>	
+
+		<meta name="keywords" content="Musma Teknik 2020" />
+		<meta name="description" content="Musma Teknik 2020">
+		<meta name="author" content="musma.net">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="/img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<!-- Web Fonts  -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light%7CPlayfair+Display:400" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="/vendor/line-awesome-1.3.0/1.3.0/css/line-awesome.min.css">
+		<link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/vendor/fontawesome-free/css/all.min.css">
+		<link rel="stylesheet" href="/vendor/animate/animate.min.css">
+		<link rel="stylesheet" href="/vendor/simple-line-icons/css/simple-line-icons.min.css">
+		<link rel="stylesheet" href="/vendor/owl.carousel/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="/vendor/owl.carousel/assets/owl.theme.default.min.css">
+        <link rel="stylesheet" href="/vendor/magnific-popup/magnific-popup.min.css">
+    
+        
+        
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="/css/theme.css">
+		<link rel="stylesheet" href="/css/theme-elements.css">
+		<link rel="stylesheet" href="/css/theme-blog.css">
+		<link rel="stylesheet" href="/css/theme-shop.css">
+
+		<!-- Current Page CSS -->
+		<link rel="stylesheet" href="/vendor/rs-plugin/css/settings.css">
+		<link rel="stylesheet" href="/vendor/rs-plugin/css/layers.css">
+		<link rel="stylesheet" href="/vendor/rs-plugin/css/navigation.css">
+		
+		<!-- Demo CSS -->
+
+
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="/css/skins/default.css"> 
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="/css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="/vendor/modernizr/modernizr.min.js"></script>
+
+    </head>
+    <body class="loading-overlay-showing" data-plugin-page-transition data-loading-overlay data-plugin-options="{'hideDelay': 500}">
+		<div class="loading-overlay">
+			<div class="bounce-loader">
+				<div class="bounce1"></div>
+				<div class="bounce2"></div>
+				<div class="bounce3"></div>
+			</div>
+		</div>
+	<body class="one-page" data-target="#header" data-spy="scroll" data-offset="100">
+
+		<div class="body">
+    @include('layouts/navigation')
+    @yield('content')
+
+    <footer id="footer" class="mt-0">
+        <div class="footer-copyright">
+            <div class="container py-2">
+                <div class="row py-4">
+                    <div class="col d-flex align-items-center justify-content-center">
+                        <p><strong>MUSMA TEKNIK 2020</strong> - © Copyright 2020. All Rights Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</div>
+
+<!-- Vendor -->
+<script src="/vendor/jquery/jquery.min.js"></script>
+<script src="/vendor/jquery.appear/jquery.appear.min.js"></script>
+<script src="/vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="/vendor/jquery.cookie/jquery.cookie.min.js"></script>
+<script src="/vendor/popper/umd/popper.min.js"></script>
+<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="/vendor/common/common.min.js"></script>
+<script src="/vendor/jquery.validation/jquery.validate.min.js"></script>
+<script src="/vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+<script src="/vendor/jquery.gmap/jquery.gmap.min.js"></script>
+<script src="/vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
+<script src="/vendor/isotope/jquery.isotope.min.js"></script>
+<script src="/vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script src="/vendor/vide/jquery.vide.min.js"></script>
+<script src="/vendor/vivus/vivus.min.js"></script>
+
+<!-- Theme Base, Components and Settings -->
+<script src="/js/theme.js"></script>
+
+<!-- Current Page Vendor and Views -->
+<script src="/vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script src="/vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script src="/js/views/view.contact.js"></script>
+
+<!-- Theme Custom -->
+<script src="/js/custom.js"></script>
+
+<!-- Theme Initialization Files -->
+<script src="/js/theme.init.js"></script>
+
+<!-- Examples -->
+<script src="/js/examples/examples.portfolio.js"></script>
+
+
+
+
+
+</body>
+</html>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,47 @@
+<header id="header" class="header-transparent header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyChangeLogo': true, 'stickyStartAt': 30, 'stickyHeaderContainerHeight': 70}">
+    <div class="header-body border-top-0 bg-dark box-shadow-none">
+        <div class="header-container container">
+            <div class="header-row">
+                <div class="header-column">
+                    <div class="header-row">
+                        <div class="header-logo">
+                            <a href="index.html">
+                                {{-- <img alt="Porto" width="82" height="40" src="img/logo-default-slim-dark.png"> --}}
+                               
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="header-column justify-content-end">
+                    <div class="header-row">
+                        <div class="header-nav header-nav-links header-nav-dropdowns-dark header-nav-light-text order-2 order-lg-1">
+                            <div class="header-nav-main header-nav-main-mobile-dark header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
+                                <nav class="collapse">
+                                    <ul class="nav nav-pills" id="mainNav">
+                                        <li class="dropdown">
+                                            <a data-hash class="dropdown-item dropdown-toggle active" href="#home">
+                                                Home
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" data-hash data-hash-offset="68" href="#tentang">tentang</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" data-hash data-hash-offset="68" href="#Polling">Poll</a>
+                                        </li>
+                                        <li>
+                                            <a class="dropdown-item" data-hash data-hash-offset="68" href="#contact">Contact Us</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main nav">
+                                <i class="fas fa-bars"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
