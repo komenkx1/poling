376ecfb01715c3027e0f69fe68521d9eeea05d28
Roogry <jodiemantra@gmail.com>
2020-12-30 23:13:01 +0800

    clean up route

--- routes/web.php
+++ routes/web.php
@@ -32,62 +32,49 @@ Route::auth();
 
 // Route::get("/register", 'RegisterController@index');
 // Route::get('register', 'RegisterController@index')->name('search');
+Route::get('/', [MainController::class, 'index'])->name('home');
+
 if (date("Y-m-d") < '2021-01-02') {
     Route::view('comingsoon', 'comingsoon')->name('comingsoon');
-    }else{
+} else {
     Route::get('register', [RegisterController::class, 'index'])->name('register');
+    Route::post('register', [RegisterController::class, 'create'])->name('register.create');
+    Route::post('register/fetch', [RegisterController::class, 'fetch'])->name('autocomplete.fetch');
     Route::get('auth', [RegisterController::class, 'index'])->name('auth');
 }
-Route::view('visi-misi', 'visi-misi');
-// Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('register/fetch', [RegisterController::class, 'fetch'])->name('autocomplete.fetch');
-Route::post('register', [RegisterController::class, 'create'])->name('register.create');
+
 Route::post('calon/fetch', [CalonController::class, 'fetch'])->name('calon.fetch');
-
-
-Route::get('/', [MainController::class, 'index'])->name('home');
-// Route::get('/role', [MainController::class, 'role'])->name('role');
-
-Route::get('/rekapitulasi', [MainController::class, 'rekap'])->name('rekap');
-Route::post('/visi-misi', [MainController::class, 'misi'])->name('visimisi');
+Route::post('visi-misi', [MainController::class, 'misi'])->name('visimisi');
+Route::get('rekapitulasi', [MainController::class, 'rekap'])->name('rekap');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/chart', [MainController::class, 'chart'])->name('chart');
-    Route::post('/vote', [MainController::class, 'vote'])->name('vote');
+    Route::get('chart', [MainController::class, 'chart'])->name('chart');
+    Route::post('vote', [MainController::class, 'vote'])->name('vote');
 
-    Route::get('/logout', [MainController::class, 'logout']);
+    Route::get('logout', [MainController::class, 'logout']);
 });
 
 Route::middleware(['role:admin|sekre'])->group(function () {
-
-    // Route::resource('calons', CalonController::class);
-    // Route::resource('mahasiswas', MahasiswaController::class)->except([
-    //     'create', 'store', 'destroy'
-    // ]);;
-    // Route::resource('prodis', ProdiController::class);
-    // Route::resource('suaras', SuaraController::class)->except([
-    //     'create', 'edit', 'update'
-    // ]);
-
     Route::get("admin", 'DashboardController@index');
+
     Route::get("admin/role/", 'RoleController@index');
     Route::put("admin/role/update/{role_model:model_id}", 'RoleController@update');
+
     Route::get("admin/calon", 'CalonController@index');
     Route::get("admin/calon/create", 'CalonController@create');
     Route::post("admin/calon/store", 'CalonController@store');
     Route::get("admin/calon/edit/{calon:id}", 'CalonController@edit');
     Route::put("admin/calon/update/{calon:id}", 'CalonController@update');
     Route::delete("admin/calon/delete/{calon:id}", 'CalonController@destroy');
+
     Route::get("admin/mahasiswa", 'MahasiswaController@index');
     Route::get("admin/mahasiswa/data", 'MahasiswaController@data');
     Route::put("admin/mahasiswa/verif/{mahasiswa:id}", 'MahasiswaController@verif');
     Route::delete("admin/mahasiswa/delete/{mahasiswa:id}", 'MahasiswaController@destroy');
+
     Route::get("admin/absen", [AbsenController::class, 'index']);
     Route::delete("admin/absen/reset/{absen:id}", [AbsenController::class, 'destroy']);
     // Route::get("admin/absen/edit/{absen:id}", [AbsenController::class, 'edit']);
     // Route::put("admin/absen/update/{absen:id}", [AbsenController::class, 'update']);
     // Route::delete("admin/absen/delete/{absen:id}", [AbsenController::class, 'destroy']);
 });
-
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
