09f10c63302f8ddc1947cb2294b8138d982485b5
Roogry <jodiemantra@gmail.com>
2020-12-30 17:25:01 +0800

    change footer

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -45,21 +45,18 @@
     <div class="app-footer">
       <div class="p-2 text-xs">
         <div class="pull-right text-muted py-1">
-          &copy; Copyright <strong>Flatkit</strong> <span class="hidden-xs-down">Built with Love v1.1.3</span>
+          &copy; Copyright <strong>Pemira Teknik</strong>
+          <span class="hidden-xs-down">Built by Mang Wahyu & Jodie</span>
           <a ui-scroll-to="content"><i class="fa fa-long-arrow-up p-x-sm"></i></a>
         </div>
         <div class="nav">
-          <a class="nav-link" href="/../">About</a>
-          <a class="nav-link" href="/http://themeforest.net/user/flatfull/portfolio?ref=flatfull">Get it</a>
+          <a class="nav-link" href="{{ route('home') }}">Home</a>
         </div>
       </div>
     </div>
     @yield('content')
   </div>
 
-
-
-  <!-- build:js scripts/app.html.js -->
   <!-- jQuery -->
   <script src="/../libs/jquery/jquery/dist/jquery.js"></script>
   <!-- Bootstrap -->
