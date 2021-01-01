30901efd40f8d587095af9c418429c127a65a1ad
komenkx <komangpermana7@gmail.com>
2021-01-02 06:48:14 +0800

    handle-upload

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -98,8 +98,13 @@ class RegisterController extends Controller
         $this->validate($request, [
             'register_nim' => 'required|min:10|max:10',
             'register_password' => 'required|min:2|max:30',
-            'file_url' => 'required|max:2000'
-        ]);
+            'file_url' => 'mimes:jpeg,jpg,png,gif|required||max:2000'
+        ],
+        [
+            'file_url.mimes' => 'format fil tidak didukung, silahkan masukan format gambar : jpeg,jpg atau png',
+            'file_url.max'=> 'Ukuran file tidak boleh lebih dari 2Mb'
+        ]
+    );
 
         //buat dapetin user dari nim yang di input
         $user = User::where('nim', $request->register_nim)->first();
